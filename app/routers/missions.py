7938b8f0ee892a06d3b140abfc714aff44bc949f
ellsphillips <elliott.phillips.dev@gmail.com>
2024-02-15 16:44:31 +0000

    Feature: list mission specific directive

--- app/routers/missions.py
+++ app/routers/missions.py
@@ -15,6 +15,11 @@ def read_root() -> list[Dataset]:
     return CATALOGUE
 
 
+@router.get("/{mission}")
+def read_mission(mission: Mission) -> list[Dataset]:
+    return [dataset for dataset in CATALOGUE if dataset.mission == mission]
+
+
 @router.get("/{mission}/{dataset_id}")
 def read_item(mission: Mission, dataset_id: str) -> Dataset:
     dataset = read_db_metric(dataset_id)
