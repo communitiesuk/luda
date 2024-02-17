8fe2d701cd24d21463d2635380dd26897f4b96a7
ellsphillips <elliott.phillips.dev@gmail.com>
2024-02-17 10:47:05 +0000

    Test: initialise luda test suite

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Test suite for the LUDA app."""
---
+++ tests/test_luda.py
@@ -0,0 +1,12 @@
+import importlib.metadata
+
+import pytest
+
+import app
+
+
+@pytest.mark.e2e()
+def test_luda_version() -> None:
+    """Test that the version is the same as in pyproject.toml."""
+    print(app.__name__)
+    assert app.__version__ == importlib.metadata.version(app.__name__)
